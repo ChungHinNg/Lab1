bf28dfcd667e7c8360d89ee3087f921a480e33f5
ChungHinNg <113311404+ChungHinNg@users.noreply.github.com>
2022-10-27 18:20:59 -0400

    Update businessContacts.router.js

--- routes/businessContacts.router.js
+++ routes/businessContacts.router.js
@@ -5,25 +5,37 @@
 var express = require('express');
 var router = express.Router();
 let contactslistController = require('../controllers/contactslist.controller')
+
+function requireAuth(req, res, next)
+{
+    // check if the user is logged in
+    if(!req.isAuthenticated())
+    {
+        req.session.url = req.originalUrl;
+        return res.redirect('/users/signin');
+    }
+    next();
+}
+
 /* Renders contact page. */
 
 router.get('/list', contactslistController.contactsListss);
 
 
 // Routers for edit
-router.get('/edit/:id', contactslistController.displayEditPage);
-router.post('/edit/:id', contactslistController.processEditPage);
+router.get('/edit/:id', requireAuth, contactslistController.displayEditPage);
+router.post('/edit/:id', requireAuth, contactslistController.processEditPage);
 
 
 /* GET Route for displaying the Add page - CREATE Operation */
-router.get('/add', contactslistController.displayAddPage);
+router.get('/add', requireAuth, contactslistController.displayAddPage);
 
 /* POST Route for processing the Add page - CREATE Operation */
-router.post('/add', contactslistController.processAddPage);
+router.post('/add', requireAuth, contactslistController.processAddPage);
 
 
 // Route for Delete
-router.get('/delete/:id', contactslistController.performDelete);
+router.get('/delete/:id', requireAuth, contactslistController.performDelete);
 
 
 
